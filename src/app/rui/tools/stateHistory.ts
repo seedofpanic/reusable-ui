87dba5c4cdea99670a8099155ff7c113a7daf8a7
Evgeny <seedofpanic@mail.ru>
2017-03-11 18:45:49 +0300

    History service for ctrl+z ctrl+shift+z

---
+++ src/app/rui/tools/stateHistory.ts
@@ -0,0 +1,49 @@
+export class IState {
+    name: string;
+    data: any;
+}
+
+export class StateHistory {
+
+    private history: IState[] = [];
+    private pointer: number;
+
+    constructor(private capacity: number = 50) {}
+
+    do(name: string, data?: any) {
+        if (this.pointer !== this.history.length - 1) {
+            this.history = this.history.slice(0, this.pointer + 1);
+        }
+
+        if (this.history.length === this.capacity) {
+            this.history.shift();
+        }
+
+        this.history.push({name, data});
+        this.pointer = this.history.length - 1;
+    }
+
+    undo(): IState {
+        if (this.pointer > -1) {
+            return this.history[this.pointer--];
+        }
+
+        return;
+    }
+
+    redo(): IState {
+        if (this.pointer < this.history.length - 1) {
+            return this.history[++this.pointer];
+        }
+
+        return;
+    }
+
+    clear(name?: string) {
+        if (name) {
+            this.history.splice(this.history.findIndex(state => state.name === name))
+        } else {
+            this.history.length = 0;
+        }
+    }
+}
